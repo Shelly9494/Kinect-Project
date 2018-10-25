36092c07cbc41a0748e5225050239ff050872642
Shelly9494 <shelly.xiao94@gmail.com>
2018-10-25 21:01:42 +1100

    Draw Lines

---
+++ Kinect/Assets/Scripts/LineController/DrawLines.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class DrawLines : MonoBehaviour {
+
+    public GameObject targetObject;
+    public GameObject brush;
+
+    private Vector3 lastPoint;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        lastPoint = targetObject.transform.position;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targetObject.activeInHierarchy == true && targetObject.transform.position != lastPoint)
+        {
+           Instantiate(brush, targetObject.transform.position, Quaternion.identity);
+            
+        }
+
+        if (targetObject.activeInHierarchy == false)
+        {
+            Invoke("StorePath",1);
+            
+        }
+
+        lastPoint = targetObject.transform.position;
+    }
+
+    void StorePath()
+    {
+        GameObject[] shapes = GameObject.FindGameObjectsWithTag("Brush");
+
+        foreach (GameObject item in shapes)
+        {
+
+            if (!item.GetComponent<FloatLetter>())
+            {
+                item.AddComponent<FloatLetter>();
+            }
+
+            Destroy(item, 20);
+        }
+        
+    }
+}
