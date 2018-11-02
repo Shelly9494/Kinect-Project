99bd628287c48fad8f04af43768dae9cc4f075c1
Adrian <44041788+iamontresor@users.noreply.github.com>
2018-11-02 11:34:12 +1100

    new version and built scene

---
+++ Interaction Project 3/Assets/SBR_ShapeChanges/Scripts/SceneManagerScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour {
+
+    Scene currentScene;
+
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+
+        if(currentScene.name == "Scn_RTBG_Brush")
+        Invoke("SwitchScene", 60);
+    }
+
+
+    public void SwitchScene()
+    {
+        Debug.Log("Switch Scene");
+        SceneManager.LoadScene("Scn_RTBG_2DBubble", LoadSceneMode.Single);
+    }
+}
